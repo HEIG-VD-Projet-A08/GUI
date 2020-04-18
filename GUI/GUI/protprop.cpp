b3c6d159f0ce5e14f75bd8e5e55b2837fce4bae4
hans-arn <44995463+hans-arn@users.noreply.github.com>
2020-04-18 16:45:25 +0200

    add validator

--- GUI/GUI/protprop.cpp
+++ GUI/GUI/protprop.cpp
@@ -5,8 +5,22 @@ ProtProp::ProtProp(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::ProtProp)
 {
-    ui->setupUi(this);
+    // set validators for input
+    QRegExpValidator *inputNumberChar = new QRegExpValidator(  QRegExp("[0-9][0-9][0-9]"));
+    QRegExpValidator *inputIteration = new QRegExpValidator(  QRegExp("[0-9][0-9][0-9][0-9][0-9]"));
 
+    // set possibility for menu with "localisation" as default possibility
+    QStringList n;
+    n << "Localisation" << "Toxicité" ;
+    ui->menu_list->addItems(n);
+
+    // enable validator for each input
+    ui->IP->setText("127.0.0.1");
+    ui->CharMax->setValidator(inputNumberChar);
+    ui->iterations->setValidator(inputIteration);
+    ui->nbWords->setValidator(inputIteration);
+
+    ui->setupUi(this);
 }
 
 ProtProp::~ProtProp()
@@ -17,10 +31,15 @@ ProtProp::~ProtProp()
 
 void ProtProp::on_btn_run_clicked()
 {
+    // load input
     nbChars = ui->CharMax->text();
     nbWords = ui->nbWords->text();
     nbIter = ui->iterations->text();
+    caract = ui->menu_list->currentText();
     ip = ui->IP->text();
+
+    if (nbChars == "" || nbWords == "" || nbIter == ""){
+    }
 }
 
 void ProtProp::on_btn_stop_clicked()
@@ -42,3 +61,4 @@ void ProtProp::on_plot_clicked()
 {
 
 }
+
